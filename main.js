52e4f1333a36800e72184d9740a29e64fd4528e1
Olivia Johnson <oliviajohnson@Olivias-Air.lan>
2023-09-22 13:58:46 +0100

    adding functionailty

--- main.js
+++ main.js
@@ -5,17 +5,16 @@
 //let weatherApi = "https://api.open-meteo.com/v1/forecast?latitude=52.4814&longitude=-1.8998&hourly=temperature_2m,relativehumidity_2m,rain,visibility"
 
 async function retriveWeather(long, lat) {
-const weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,rain,visibility`, {
+const weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max,windspeed_10m_max&timezone=GMT`, {
     headers: {
         Accept: "application/json"
         }
     })
 let data = await weather.json()
 console.log(data)
+setWeather(data)
 return data
 }
-
-let city = "gloucester"
 
 async function retrieveGeo(cityIn) {
     const weather = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityIn}`, {
@@ -31,20 +30,40 @@ async function retrieveGeo(cityIn) {
     return data
     }
 
-    retrieveGeo(city)
-
 
 //select the relevent elements in html to input the info from the api
     //Temp
+    let temp = document.getElementById("tempTxt")
     //Max and min temps
+    let maxTemp = document.getElementById("maxTemp")
+    let minTemp = document.getElementById("minTemp")
     //precipitation 
+    let precip = document.getElementById("precip")
     //wind speed
+    let windSpeed = document.getElementById("windSpeed")
     //humidity
+    let weatherCode = document.getElementById("weatherCode")
 
 //set the innerText of each of the above to the relevent info from the API
+  
+ function setWeather(input){
     //Temp
+    temp.innerText = (input.daily.temperature_2m_max[0] + input.daily.temperature_2m_min[0])/2 + input.daily_units.temperature_2m_max
     //Max and min temps
+    maxTemp.innerText = input.daily.temperature_2m_max[0] + input.daily_units.temperature_2m_max
+    minTemp.innerText = input.daily.temperature_2m_min[0] + input.daily_units.temperature_2m_min
     //precipitation 
+    precip.innerText = input.daily.precipitation_probability_max[0] + input.daily_units.precipitation_probability_max
     //wind speed
-    //humidity
+    windSpeed.innerText = input.daily.windspeed_10m_max[0] + input.daily_units.windspeed_10m_max
+    //weathercode
+    weatherCode.innerText =  input.daily.weathercode[0]
+}
 
+let locationBtn = document.getElementById("inputBtn")
+let locationInput = document.getElementById("locationIn")
+locationBtn.addEventListener("click", inputLocation)
+
+function inputLocation(){
+    retrieveGeo(locationInput.value)
+}
