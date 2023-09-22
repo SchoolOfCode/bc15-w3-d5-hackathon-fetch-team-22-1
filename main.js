62051977e76d2407da2176ab23b1ab219ef3f76c
Olivia Johnson <oliviajohnson@Olivias-Air.lan>
2023-09-22 15:55:03 +0100

    cleaned up js

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-// find the api link
-//work out what info we want 
-//check what info comes through 
-// link the API with async function
-//let weatherApi = "https://api.open-meteo.com/v1/forecast?latitude=52.4814&longitude=-1.8998&hourly=temperature_2m,relativehumidity_2m,rain,visibility"
+//find the api link
+//work out what info we want to get from weather api
+//check how info comes through so we know how to select and link it with DOM
+//use Geo api to convert cities to lat and long? 
+//select elements from html that we want to be dynamic 
+//select info that we need from the API response
+//link html elements with .innerText to the the API responses
 
+//stretch goals
+//add input for location - add html code
+//add event to input -> functino to set city = input.value
+//call geo function with (city) to convert city to lat and long
+
+//add dynamic background images
+//based on weathe codes?
+
+//function to fetch weather info from the API
 async function retriveWeather(long, lat) {
 const weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max,windspeed_10m_max&timezone=GMT`, {
     headers: {
@@ -11,11 +22,11 @@ const weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${l
         }
     })
 let data = await weather.json()
-console.log(data)
 setWeather(data)
 return data
 }
 
+//function to convert cities to lat and long using Geo api
 async function retrieveGeo(cityIn) {
     const weather = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityIn}`, {
         headers: {
@@ -23,64 +34,61 @@ async function retrieveGeo(cityIn) {
         }
     })
     let data = await weather.json()
+    //set latitude and longitude to be passed to the weatherAPI
     let lat = data.results[0].latitude
     let long = data.results[0].longitude
 
+    //call weather API with lat and long
     retriveWeather(long, lat)
     return data
-    }
+}
 
 
 //select the relevent elements in html to input the info from the api
-    //Temp
-    let temp = document.getElementById("tempTxt")
-    //Max and min temps
-    let maxTemp = document.getElementById("maxTemp")
-    let minTemp = document.getElementById("minTemp")
-    //precipitation 
-    let precip = document.getElementById("precip")
-    //wind speed
-    let windSpeed = document.getElementById("windSpeed")
-    //humidity
-    let weatherCode = document.getElementById("weatherCode")
-    //location 
-    let locationTxt = document.getElementById("location")
-    //info div for setting background pic
-    let infoDiv = document.getElementById("info")
+let temp = document.getElementById("tempTxt")
+let maxTemp = document.getElementById("maxTemp")
+let minTemp = document.getElementById("minTemp")
+let precip = document.getElementById("precip")
+let windSpeed = document.getElementById("windSpeed")
+let weatherCode = document.getElementById("weatherCode")
+let locationTxt = document.getElementById("location")
+let infoDiv = document.getElementById("info")
 
 //set the innerText of each of the above to the relevent info from the API
-  
 function setWeather(input){
-    //Temp
+    //Temp - working out average based on min and max that day
     temp.innerText = ((input.daily.temperature_2m_max[0] + input.daily.temperature_2m_min[0])/2).toFixed(2) + input.daily_units.temperature_2m_max
-    //Max and min temps
     maxTemp.innerText = input.daily.temperature_2m_max[0] + input.daily_units.temperature_2m_max
     minTemp.innerText = input.daily.temperature_2m_min[0] + input.daily_units.temperature_2m_min
-    //precipitation 
     precip.innerText = input.daily.precipitation_probability_max[0] + input.daily_units.precipitation_probability_max
-    //wind speed
     windSpeed.innerText = input.daily.windspeed_10m_max[0] + input.daily_units.windspeed_10m_max
     //weathercode
     let weathCode = input.daily.weathercode[0]
-    console.log(weathCode)
+    //find right weather code description and image
     for(let i = 0; i < weatherCodeDescriptAndImgs.length ; i++){
+        //check if id of weathercode from API response matches against the descriptions ID in the array
     if(weatherCodeDescriptAndImgs[i].id.includes(weathCode)){
+        //if match - set weathercode txt to that description 
     weatherCode.innerText = weatherCodeDescriptAndImgs[i].description
+        //set background image to relevant img
     infoDiv.style.backgroundImage = `url(${weatherCodeDescriptAndImgs[i].img})`}
     }
-    //location
-
 }
 
+//setting up input for user to be able to search different cities
 let locationBtn = document.getElementById("inputBtn")
 let locationInput = document.getElementById("locationIn")
+//event listened to check when go button is clicked
 locationBtn.addEventListener("click", inputLocation)
 
+//function to call the API functions with an argument of the city that has been input by user
 function inputLocation(){
     retrieveGeo(locationInput.value)
-
+    //set location text to match the user input
     locationTxt.innerText = locationInput.value
 }
+
+//Array to hold weather code descriotions and relevant images
 
 let weatherCodeDescriptAndImgs =[
     {id: [0],
