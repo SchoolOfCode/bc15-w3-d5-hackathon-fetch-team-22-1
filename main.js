757183ede25c7328660ef42012728e4509bf90fa
Olivia Johnson <oliviajohnson@Olivias-Air.lan>
2023-09-22 12:21:43 +0100

    geoAPI

--- main.js
+++ main.js
@@ -2,34 +2,38 @@
 //work out what info we want 
 //check what info comes through 
 // link the API with async function
-async function retriveWeather() {
-const weather = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.4814&longitude=-1.8998&hourly=temperature_2m,relativehumidity_2m,rain,visibility", {
-    headers: {
-    Accept: "application/json"
-    }
-})
+//let weatherApi = "https://api.open-meteo.com/v1/forecast?latitude=52.4814&longitude=-1.8998&hourly=temperature_2m,relativehumidity_2m,rain,visibility"
 
+async function retriveWeather(long, lat) {
+const weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,rain,visibility`, {
+    headers: {
+        Accept: "application/json"
+        }
+    })
 let data = await weather.json()
 console.log(data)
 return data
-
 }
-//retriveWeather()
 
-let city = "berlin"
+let city = "gloucester"
 
-async function retrieveGeo(city) {
-    const weather = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=berlin", {
+async function retrieveGeo(cityIn) {
+    const weather = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityIn}`, {
         headers: {
             Accept: "application/json"
         }
     })
     let data = await weather.json()
-    console.log(data)
+    let lat = data.results[0].latitude
+    let long = data.results[0].longitude
+
+    retriveWeather(long, lat)
     return data
     }
 
-    retrieveGeo()
+    retrieveGeo(city)
+
+
 //select the relevent elements in html to input the info from the api
     //Temp
     //Max and min temps
