acf2a95b6cd2581998224cae376d43e83ec0f8f5
Christener19 <christenerleigh7070@gmail.com>
2023-09-22 11:11:34 +0100

    basic code added

---
+++ main.js
@@ -0,0 +1,19 @@
+// link the API with async function 
+async function retriveWeather() {
+const weather = await fetch("", {
+    headers: {
+    Accept: "application/json"
+    }
+})
+}
+
+
+
+async function () {
+    const weather = await fetch("", {
+        headers: {
+        Accept: "application/json"
+        }
+    })
+    }
+
